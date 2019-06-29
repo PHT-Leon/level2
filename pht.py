350c15dca503c1abc4d7c67e921a8589e69354e6
PHT-Leon <52360940+PHT-Leon@users.noreply.github.com>
2019-06-30 02:33:44 +0300

    Update pht.py

--- pht.py
+++ pht.py
@@ -4,3 +4,10 @@ name = str(input("What's Your Name?: "))
 age = input("How Old You: ")
 
 print("Your Name is:",name,"\nYour Age is ",age)
+email = str(input("Write Your Email"))
+
+password = int(input("Write Your Password"))
+
+
+
+print("Your Email is",email,"Your Password is",password)
